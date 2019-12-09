0629b7994cd91d716c16f885fce9f5f29fbb1942
trallocnivek242 <collart.1510.student@ifosupwawre.be>
2019-12-09 16:14:59 +0100

    site ossature
    debut d'ossature du site

---
+++ site_web/index.php
@@ -0,0 +1,19 @@
+<?php 
+    require_once 'includes/fonctions.php';
+    $page = $url_data('page') ? $url_data('page') : 'accueil';
+?>
+<!DOCTYPE html>
+    <html lang="fr">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Projet architect - <?=$page;?></title>
+    </head>
+    <body>
+        <?php include_once 'ossature/header.php';?>
+        <?php include_once 'ossature/nav.php';?>
+        <?php include_once 'ossature/main.php';?>
+        <?php include_once 'ossature/footer.php';?>
+    </body>
+</html>
